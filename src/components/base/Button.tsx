ffe585f342676613ba9414ddd3d7fff3faeb2ee0
kianfarzaneh <kianfarzaneh1380@gmail.com>
2025-04-06 17:11:42 +0330

    fix: add transition to button

--- src/components/base/Button.tsx
+++ src/components/base/Button.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from "react";
 
 interface IProps {
-    content : string
-    className : string
+  content: string;
+  className: string;
 }
 
-export default function Button({content,className} : IProps) {
+export default function Button({ content, className }: IProps) {
   return (
-    <button className={`bg-[#EC8C2F] text-white px-[107px] py-3 rounded-xl ${className}`}>
-        {content}
+    <button
+      className={`
+        bg-[#EC8C2F] text-white px-[107px] py-3 rounded-xl
+        transition-all duration-200 ease-in-out
+        hover:bg-[#d4771a] 
+        active:bg-[#bb6817] active:scale-95
+        ${className}
+      `}
+    >
+      {content}
     </button>
-  )
+  );
 }
