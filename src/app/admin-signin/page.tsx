87b272ef6975141564e3fd3ed43055e6d371e98c
kianfarzaneh <kianfarzaneh1380@gmail.com>
2025-04-06 15:54:33 +0330

    fix: determine type of validation for signin component by props

--- src/app/admin-signin/page.tsx
+++ src/app/admin-signin/page.tsx
@@ -3,6 +3,6 @@ import React from "react";
 
 export default function AdminSignin() {
   return <div className="w-full h-screen bg-[#EC8C2F] p-2">
-    <Signin />
+    <Signin validationType="admin"/>
   </div>;
 }
