433c30be48dd0b3aa1250d1cc9260092069f89ba
kianfarzaneh <kianfarzaneh1380@gmail.com>
2025-04-10 15:21:16 +0330

    fix:delete an Icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import HomePageFilter from "@/components/filters/HomePageFilter";
 import { CgProfile } from "react-icons/cg";
 import { CiSearch } from "react-icons/ci";
 import { FaCarAlt } from "react-icons/fa";
 import { FiHeart, FiHome } from "react-icons/fi";
 import { IoIosNotificationsOutline } from "react-icons/io";
-import perCommen from "@/localization/persian/common.json"
+import perCommen from "@/localization/persian/common.json";
 
 export default function Home() {
   return (
@@ -24,14 +23,17 @@ export default function Home() {
             </div>
           </div>
           <div className="search-box flex items-center justify-center mt-9 ">
-            <button className="bg-white text-[#ec8c2f] ml-[-3px] pr-3 h-12 rounded-r-lg"><CiSearch size={34} /></button>
-            <input type="text" className="bg-white outline-none w-[290px] pr-2 h-12 rounded-l-lg" />
+            <button className="bg-white text-[#ec8c2f] ml-[-3px] pr-3 h-12 rounded-r-lg">
+              <CiSearch size={34} />
+            </button>
+            <input
+              type="text"
+              className="bg-white outline-none w-[290px] pr-2 h-12 rounded-l-lg"
+            />
           </div>
         </header>
-        <HomePageFilter />
-
       </div>
-      
+
       <footer className="footer p-4 flex justify-center items-center gap-8 w-full h-auto rounded-t-4xl shadow-black shadow-2xl drop-shadow-lg">
         <div className="flex flex-col justify-center items-center gap-1 hover:text-[#ec8c2f] active:text-[#ec8c2f] transition-colors duration-200">
           <FiHome strokeWidth={1} className="w-[30px] h-[30px]" />
