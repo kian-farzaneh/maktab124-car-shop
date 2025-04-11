00c05f56d517e7e3beb5313c2c083509d4f9c481
kianfarzaneh <kianfarzaneh1380@gmail.com>
2025-04-11 17:41:46 +0330

    feat: set functionality of dashboard's layout's button

--- src/layout/dashboard-layout/Footer.tsx
+++ src/layout/dashboard-layout/Footer.tsx
@@ -1,18 +1,52 @@
+"use client";
 import React from "react";
 import persian from "@/localization/persian/dashboard/main-page.json";
+import { useRouter } from "next/navigation";
 
 interface IProps {}
 
 export default function Footer({}: IProps) {
+  const router = useRouter();
+
   return (
     <footer
       style={{ boxShadow: "  0 -4px 10px rgba(0, 0, 0, 0.3)" }}
       className="bg-white rounded-t-full h-11 flex justify-center items-center"
     >
       <div className="w-3xl flex justify-center items-center gap-16 text-[#6e6e6e]">
-        <button className="hover:text-[#ec8c2f] active:text-[#ec8c2f] transition-colors duration-200 cursor-pointer">{persian.products}</button>
-        <button className="hover:text-[#ec8c2f] active:text-[#ec8c2f] transition-colors duration-200 cursor-pointer">{persian.inventoryAndPrices}</button>
-        <button className="hover:text-[#ec8c2f] active:text-[#ec8c2f] transition-colors duration-200 cursor-pointer">{persian.orders}</button>
+        <button
+          onClick={() => {
+            router.push("/dashboard");
+          }}
+          className="hover:text-[#ec8c2f] active:text-[#ec8c2f] transition-colors duration-200 cursor-pointer"
+        >
+          {persian.dashboard}
+        </button>
+
+        <button
+          onClick={() => {
+            router.push("/dashboard/products");
+          }}
+          className="hover:text-[#ec8c2f] active:text-[#ec8c2f] transition-colors duration-200 cursor-pointer"
+        >
+          {persian.products}
+        </button>
+        <button
+          onClick={() => {
+            router.push("/dashboard/inventory-price");
+          }}
+          className="hover:text-[#ec8c2f] active:text-[#ec8c2f] transition-colors duration-200 cursor-pointer"
+        >
+          {persian.inventoryAndPrices}
+        </button>
+        <button
+          onClick={() => {
+            router.push("/dashboard/orders");
+          }}
+          className="hover:text-[#ec8c2f] active:text-[#ec8c2f] transition-colors duration-200 cursor-pointer"
+        >
+          {persian.orders}
+        </button>
       </div>
     </footer>
   );
--- src/layout/dashboard-layout/Header.tsx
+++ src/layout/dashboard-layout/Header.tsx
@@ -1,17 +1,36 @@
+"use client";
 import React from "react";
 import { CiLogout } from "react-icons/ci";
-import persian from "@/localization/persian/dashboard/main-page.json"
+import persian from "@/localization/persian/dashboard/main-page.json";
+import { usePathname } from "next/navigation";
+import { useDispatch } from "react-redux";
+import { removeAccessToken } from "@/redux/slices/dashboard/adminAuthSlice";
 
 interface IProps {}
 
 export default function Header({}: IProps) {
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(removeAccessToken());
+  };
+  const pathname = usePathname();
+  const getTitle = () => {
+    if (pathname === "/dashboard") return persian.mainTitle;
+    if (pathname === "/dashboard/products") return persian.products;
+    if (pathname === "/dashboard/inventory-price")
+      return persian.inventoryAndPrices;
+    if (pathname === "/dashboard/orders") return persian.orders;
+  };
   return (
     <header
       dir="ltr"
       className="bg-[#ec8c2f] rounded-b-full h-11 shadow-[#ec8c2f] shadow-2xl flex justify-between items-center"
     >
-      <CiLogout className="text-white text-3xl ml-3 mb-1 transition-all duration-300 hover:text-[#ec8c2f] hover:bg-white rounded-full p-1 hover:scale-110 cursor-pointer" />
-      <h1>{persian.mainTitle}</h1>
+      <CiLogout
+        onClick={handleLogout}
+        className="text-white text-3xl ml-3 mb-1 transition-all duration-300 hover:text-[#ec8c2f] hover:bg-white rounded-full p-1 hover:scale-110 cursor-pointer"
+      />
+      <h1 className="font-bold text-2xl">{getTitle()}</h1>
       <div></div>
     </header>
   );
