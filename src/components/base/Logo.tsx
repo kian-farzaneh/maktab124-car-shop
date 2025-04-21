5d062fd9d5e823b04d1e82fc6d06ad30a6f11d9a
kianfarzaneh <kianfarzaneh1380@gmail.com>
2025-04-21 21:19:13 +0330

    feat: create logo component

---
+++ src/components/base/Logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <span className="flex justify-center items-center py-2 rounded-3xl bg-white w-36">
+        <img src="/logo/LOGO.png" alt="logo" className="w-30" />
+    </span>
+  )
+}
