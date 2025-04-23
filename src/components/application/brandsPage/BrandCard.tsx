314e8ac9ca998f1a6b9a5f58161120c6d0508d12
kianfarzaneh <kianfarzaneh1380@gmail.com>
2025-04-23 14:44:20 +0330

    feat: set functionality of brands page

--- src/components/application/brandsPage/BrandCard.tsx
+++ src/components/application/brandsPage/BrandCard.tsx
@@ -1,22 +1,26 @@
 import React from "react";
-
+import Link from "next/link";
 interface BrandCardProps {
   name: string;
   image: string;
   imgClassName: string;
+  slug: string;
 }
 
-export default function BrandCard({ name, image, imgClassName }: BrandCardProps) {
+export default function BrandCard({
+  name,
+  image,
+  imgClassName,
+  slug,
+}: BrandCardProps) {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="shadow-gray-300 shadow-xl rounded-2xl transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl">
-        <img
-          src={image}
-          alt={`${name}_logo`}
-          className={`${imgClassName}`}
-        />
+    <Link href={`/brands/${slug}`}>
+      <div className="flex flex-col justify-center items-center">
+        <div className="shadow-gray-300 shadow-xl rounded-2xl transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl">
+          <img src={image} alt={`${name}_logo`} className={`${imgClassName}`} />
+        </div>
+        <p className="text-[25px]">{name}</p>
       </div>
-      <p className="text-[25px]">{name}</p>
-    </div>
+    </Link>
   );
 }
--- src/components/application/brandsPage/BrandsMainComponent.tsx
+++ src/components/application/brandsPage/BrandsMainComponent.tsx
@@ -13,12 +13,12 @@ export default function BrandsMainComponent() {
         <Logo />
       </header>
       <div className="flex-1 overflow-y-auto scrollbar-hide grid grid-cols-3">
-        <BrandCard name={persian.Benz} image={"/brands/Benz.png"} imgClassName='w-[300px]'/>
-        <BrandCard name={persian.Bentley} image={"/brands/Bentley.png"} imgClassName='w-[300px] mx-[20px] my-[93px]'/>
-        <BrandCard name={persian.Lamborgini} image={"/brands/Lamborghini.png"} imgClassName='w-[177px] my-[50px] mx-[47px]' />
-        <BrandCard name={persian.Ford} image={"/brands/Ford.png"} imgClassName='w-[205px] my-[90px] mx-[47px]' />
-        <BrandCard name={persian.Hummer} image={"/brands/Hummer.png"} imgClassName='w-[250px] my-[110px] mx-[40px]' />
-        <BrandCard name={persian.Audi} image={"/brands/Audi.png"} imgClassName='w-[300px] my-[20px]' />
+        <BrandCard name={persian.Benz} image={"/brands/Benz.png"} imgClassName='w-[300px]' slug="Benz" />
+        <BrandCard name={persian.Bentley} image={"/brands/Bentley.png"} imgClassName='w-[300px] mx-[20px] my-[93px]' slug="Bentley" />
+        <BrandCard name={persian.Lamborgini} image={"/brands/Lamborghini.png"} imgClassName='w-[177px] my-[50px] mx-[47px]' slug="Lamborghini" />
+        <BrandCard name={persian.Ford} image={"/brands/Ford.png"} imgClassName='w-[205px] my-[90px] mx-[47px]' slug="Ford" />
+        <BrandCard name={persian.Hummer} image={"/brands/Hummer.png"} imgClassName='w-[250px] my-[110px] mx-[40px]' slug="Hummer" />
+        <BrandCard name={persian.Audi} image={"/brands/Audi.png"} imgClassName='w-[300px] my-[20px]' slug="Audi" />
       </div>
     </div>
   )
