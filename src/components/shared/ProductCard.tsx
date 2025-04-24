a7f407bdff75f4ddeb87647f209a2d3bc88d2247
kianfarzaneh <kianfarzaneh1380@gmail.com>
2025-04-24 16:31:08 +0330

    feat: create product card component

---
+++ src/components/shared/ProductCard.tsx
@@ -0,0 +1,43 @@
+import { convertToPersianNumbers } from "@/utils/convertToPersianNumbers/convertToPersianNumbers";
+import React from "react";
+import { BsFillFuelPumpDieselFill } from "react-icons/bs";
+import { MdOutlineAirlineSeatReclineNormal } from "react-icons/md";
+import { TbManualGearboxFilled } from "react-icons/tb";
+
+export default function ProductCard(prop: { price: any; model: any; brand: any; gearbox: any; fuelType: any; seats: any; image: any; name:any }) {
+
+    const {price,model,brand,gearbox,fuelType,seats,image,name} = prop
+
+  return (
+    <div className="w-[400px] border border-[#ff8307] rounded-2xl h-[280px]">
+      <div className="px-5 pt-2.5">
+        <div>
+          <img src={image} alt={`${brand} ${model}`} className="w-96 h-[200px]"/>
+        </div>
+
+        <div className="flex items-center justify-between py-2">
+          <p>
+            <span className="font-bold">$</span>
+            <span>{convertToPersianNumbers(price)}</span>
+          </p>
+          <p className="font-bold ">{name}</p>
+        </div>
+      </div>
+
+      <div className="p-1 flex justify-around items-center bg-[#ff8307] text-white rounded-b-2xl">
+        <div className="flex justify-center">
+          <TbManualGearboxFilled size={22} />
+          <p>{gearbox}</p>
+        </div>
+        <div className="flex justify-center gap-1">
+          <BsFillFuelPumpDieselFill size={22} />
+          <p>{fuelType}</p>
+        </div>
+        <div className="flex justify-center">
+          <MdOutlineAirlineSeatReclineNormal size={24} />
+          <p>{convertToPersianNumbers(seats)}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
