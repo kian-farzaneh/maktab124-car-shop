d6133ef38894a2ad65ea1f14061e638489cc9d15
kianfarzaneh <kianfarzaneh1380@gmail.com>
2025-04-24 16:32:41 +0330

    feat: render products of each brand in dynamic brand page

--- src/app/brands/[brand]/page.tsx
+++ src/app/brands/[brand]/page.tsx
@@ -2,6 +2,8 @@
 
 import React, { useEffect, useState } from "react";
 import { getProductsByBrand } from "@/api/getProductsByBrand";
+import Logo from "@/components/base/Logo";
+import ProductCard from "@/components/shared/ProductCard";
 
 type Props = {
   params: {
@@ -9,8 +11,34 @@ type Props = {
   };
 };
 
+type IProduct = {
+  id: string;
+  name: string;
+  brand: string;
+  model: string;
+  year: string;
+  price: string;
+  discountPercentage: string;
+  discountedPrice: string;
+  category: string;
+  colors: string[];
+  stock: string;
+  condition: string;
+  isAvailable: boolean;
+  image: string;
+  gallery: string[];
+  description: string;
+  fuelType: string;
+  gearbox: string;
+  seats: string;
+  views: string;
+  ratings: any[];
+  comments: any[];
+  createdAt: string;
+};
+
 export default function BrandPage({ params }: Props) {
-  const [product, setProduct] = useState();
+  const [product, setProduct] = useState<IProduct[]>();
 
   const brandMap: Record<string, string> = {
     Ford: "فورد",
@@ -32,10 +60,40 @@ export default function BrandPage({ params }: Props) {
         console.error("خطا در گرفتن دیتا:", err);
       });
   }, []);
+  useEffect(() => {
+    console.log("محصولات دریافت‌شده:", product);
+  }, [product]);
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">ماشین شماره: {params.brand}</h1>
-    </div>
+    <>
+      <header
+        dir="ltr"
+        className="bg-[url('/header/header.png')] bg-cover bg-center h-20 w-full pl-2 flex items-center"
+      >
+        <Logo />
+        <h1 className="ml-[460px] text-4xl font-bold">
+          {brandMap[params.brand]}
+        </h1>
+      </header>
+      <div
+        className="grid grid-cols-3 gap-y-12 h-[calc(100vh-80px)] overflow-y-auto scrollbar-hide pl-10 py-5"
+        dir="ltr"
+      >
+        {product?.map((item) => (
+          <div key={item.id}>
+            <ProductCard
+              price={Number(item.discountedPrice)}
+              model={item.model}
+              brand={item.brand}
+              gearbox={item.gearbox}
+              fuelType={item.fuelType}
+              seats={Number(item.seats)}
+              image={item.image}
+              name={item.name}
+            />
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
