a7aac30e753210cce4d207041cd4e5b3d32a078c
kianfarzaneh <kianfarzaneh1380@gmail.com>
2025-04-26 23:06:47 +0330

    feat: enable ability of search in  home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"; 
+"use client";
 
 import { CgProfile } from "react-icons/cg";
 import { CiSearch } from "react-icons/ci";
@@ -14,11 +14,12 @@ import ProductCard from "@/components/shared/ProductCard";
 
 export default function Home() {
   const [products, setProducts] = useState<any[]>([]);
-
+  const [searchText, setSearchText] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    getProducts()
+  const fetchProducts = (searchValue?: string) => {
+    setIsLoading(true);
+    getProducts(searchValue ? { searchKey: "name", searchValue } : {})
       .then((res) => {
         setProducts(res);
       })
@@ -26,14 +27,21 @@ export default function Home() {
         console.error(err);
       })
       .finally(() => {
-        setIsLoading(false); 
+        setIsLoading(false);
       });
+  };
+
+  useEffect(() => {
+    fetchProducts();
   }, []);
+
+  const handleSearch = () => {
+    fetchProducts(searchText);
+  };
 
   return (
     <div className="flex flex-col h-screen">
       {" "}
-
       <header className="bg-[#ec8c2f] py-3 px-1">
         <div dir="ltr" className="flex items-center justify-between px-3">
           <div className="logo flex items-center gap-1">
@@ -50,17 +58,21 @@ export default function Home() {
         </div>
 
         <div className="search-box flex items-center justify-center mt-9">
-          <button className="bg-white text-[#ec8c2f] ml-[-3px] pr-3 h-12 rounded-r-lg">
+          <button
+            onClick={handleSearch}
+            className="bg-white text-[#ec8c2f] ml-[-3px] pr-3 h-12 rounded-r-lg"
+          >
             <CiSearch size={34} />
           </button>
           <input
             type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
             placeholder="جستجو کنید..."
             className="bg-white outline-none w-[290px] pr-2 h-12 rounded-l-lg"
           />
         </div>
       </header>
-
       <div className="flex-1 overflow-y-auto p-4 scrollbar-hide">
         {isLoading ? (
           <div className="text-center text-[#ec8c2f] mt-10 font-semibold">
@@ -76,7 +88,7 @@ export default function Home() {
               <div className="grid grid-cols-3 gap-4">
                 {products.map((item: any) => (
                   <ProductCard
-                    key={item.id} 
+                    key={item.id}
                     id={item.id}
                     price={Number(item.discountedPrice)}
                     model={item.model}
@@ -93,8 +105,6 @@ export default function Home() {
           </>
         )}
       </div>
-
-
       <footer className="footer p-4 flex justify-around items-center w-full h-auto rounded-t-4xl shadow-black shadow-2xl drop-shadow-lg bg-white">
         <div className="flex flex-col justify-center items-center gap-1 hover:text-[#ec8c2f] transition-colors duration-200 cursor-pointer">
           <FiHome strokeWidth={1} className="w-[30px] h-[30px]" />
@@ -116,7 +126,6 @@ export default function Home() {
           <p className="text-sm">{perCommen.Profile}</p>
         </div>
       </footer>
-
     </div>
   );
 }
