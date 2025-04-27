af694abd890b94d02b69390c2cc16a700d17bf37
kianfarzaneh <kianfarzaneh1380@gmail.com>
2025-04-27 21:23:23 +0330

    fix: set the user sign in scrollabel

--- src/app/user-signin/page.tsx
+++ src/app/user-signin/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function UserSigninPage() {
   return (
-    <div className="w-full h-screen bg-[url('/header/header.png')] p-2">
+    <div className="w-full h-screen bg-[url('/header/header.png')] p-2 overflow-y-auto scrollbar-hide">
         <Signin validationType='user' />
     </div>
   )
