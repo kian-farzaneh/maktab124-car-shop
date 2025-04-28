161ab1796fe610fb22f527c9978a2cb92e89d32f
kianfarzaneh <kianfarzaneh1380@gmail.com>
2025-04-28 20:01:33 +0330

    feat: add tostify library to the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReduxProvider from "@/components/base/ReduxProvider";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +32,7 @@ export default function RootLayout({
       >
         <ReduxProvider>
           <div className="h-full">{children}</div>
+          <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={true} pauseOnFocusLoss draggable pauseOnHover />
         </ReduxProvider>
       </body>
     </html>
